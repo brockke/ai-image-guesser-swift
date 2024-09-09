cbe2421a6ffd3af6d272ee61b81a7e4d143adecd
Brock Kennedy <brock97k@gmail.com>
2024-09-08 23:53:53 -0300

    Initial Commit

---
+++ Job Interview Test App/Job_Interview_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Job_Interview_Test_AppApp.swift
+//  Job Interview Test App
+//
+//  Created by Brock Kennedy on 2024-09-08.
+//
+
+import SwiftUI
+
+@main
+struct Job_Interview_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
