d01823616f014654a2a4a946c34ca788ab81d4be
Brock Kennedy <brock97k@gmail.com>
2024-09-18 06:00:24 -0300

    Inital skeleton of app

--- Job Interview Test App/ContentView.swift
+++ Job Interview Test App/ContentView.swift
@@ -9,14 +9,73 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+      Captcha()
+    }
+}
+
+struct Captcha: View {
+  var body: some View {
+    HStack{
+      Spacer()
+      VStack{
+        HStack{
+          VStack(alignment: .leading){
+            Text("Select all images with")
+            Text("ai art")
+              .fontWeight(.heavy)
+            Text("Click verify once there are none left")
+          }
+          Spacer()
         }
         .padding()
+        .foregroundColor(.white)
+        VStack{
+          HStack{
+            CaptchaBox()
+            CaptchaBox()
+            CaptchaBox()
+          }
+          HStack{
+            CaptchaBox()
+            CaptchaBox()
+            CaptchaBox()
+          }
+          HStack{
+            CaptchaBox()
+            CaptchaBox()
+            CaptchaBox()
+          }
+        }
+        .background(.white)
+        HStack{
+          Button(action: {}){
+            Text("refresh")
+          }
+          Button(action: {}){
+            Text("github")
+          }
+          Spacer()
+          Button(action: {}){
+            Text("verify")
+          }
+        }
+        .padding()
+        .foregroundColor(.white)
+      }
+      .background(.blue)
+      Spacer()
     }
+    
+  }
+}
+
+struct CaptchaBox: View {
+  var body: some View{
+    Button(action: {}) {
+      Rectangle()
+        .frame(width: 100, height: 100)
+    }
+  }
 }
 
 #Preview {
